edad6b56e03691a8607471a21e91097685e88bcc
mohdhanif1234 <hanif.topia@gmail.com>
2021-12-06 01:11:11 +0530

    [Hanif]Add: Created a trigger for deleting a trigger and display the result with the deleted keyword

--- Triggers.sql
+++ Triggers.sql
@@ -1,5 +1,6 @@
 select * from User_Details;
 
+-- DML Triggers
 -- creating a trigger for insert
 create trigger tr_Candidate_ForInsert
 on User_Details
@@ -23,3 +24,19 @@ end
 
 -- inserting the data in the table for activating the trigger
 insert into User_Details values('vidya@gmail.com','Vidya','Sagar','4854dcrdre@yu',11641116,1);
+
+-- creating a delete trigger
+create trigger tr_Candidate_ForDelete
+on User_Details
+after delete
+as 
+begin
+	select * from deleted
+end
+
+-- retrieving the data from the table
+select * from User_Details;
+
+-- deleting theb data from the table and displaying the deleted data with the help of deleted keyword
+delete from User_Details where id = 5;
+delete from User_Details where id = 6;
