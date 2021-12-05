42f9906bdbc930b97965dd5ad90aa36ed196271a
mohdhanif1234 <hanif.topia@gmail.com>
2021-12-05 12:07:48 +0530

    [Hanif]Add: Added a query to create a function with a multiple parameters

--- Functions.sql
+++ Functions.sql
@@ -22,3 +22,16 @@ end
 
 -- calling the function"
 select dbo.TakeANumber(5);
+
+-- creating a scalar function with a multiple parameter
+create function Substraction(@num1 as int, @num2 as int)
+returns int
+as 
+begin
+return(@num1 - @num2)
+end
+
+-- calling the function"
+select dbo.Substraction(5,10);
+select dbo.Substraction(5,6);
+select dbo.Substraction(5,1);
