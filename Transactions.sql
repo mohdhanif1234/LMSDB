d8fbc59e747781e1ebb3fb71b0a79569854b1bf1
mohdhanif1234 <hanif.topia@gmail.com>
2021-12-06 19:44:20 +0530

    [Hanif]Add: Added a simple query to implement the concept of exception handling in SQL Server

--- Transactions.sql
+++ Transactions.sql
@@ -1,8 +1,9 @@
 select * from Hired_Candidates;
 
+-- TRANSACTIONS IN SQL SERVER
 -- beginning a transaction 
-begin transaction
-update Hired_Candidates set first_name = 'Murtuza', last_name = 'Nullwala' where id = 5;
+begin transaction --explicit transaction 
+update Hired_Candidates set first_name = 'Murtuza', last_name = 'Nullwala' where id = 4;
 
 -- committing a transaction
 commit transaction; --uncommitted data is not saved in other connection
@@ -11,3 +12,13 @@ rollback transaction; --undo
 
 -- delete transaction
 delete Hired_Candidates where id = 5;
+
+-- TRY CATCH IN SQL SERVER
+ 
+ begin try
+ select 10/0
+ select * from Hired_Candidates
+ end try
+ begin catch
+	print 'You cannot divide a number by zero'
+ end catch
