ab55bcc4ce2283d0dfd508d59f14ed4d44737ed7
mohdhanif1234 <hanif.topia@gmail.com>
2021-12-06 19:10:32 +0530

    [Hanif]Add: Added a query to rollback a previously inititated transaction

--- Transactions.sql
+++ Transactions.sql
@@ -2,7 +2,9 @@ select * from Hired_Candidates;
 
 -- beginning a transaction 
 begin transaction
-update Hired_Candidates set first_name = 'Ali', last_name = 'Bhai' where id = 5;
+update Hired_Candidates set first_name = 'Murtuza', last_name = 'Nullwala' where id = 5;
 
 -- committing a transaction
-commit transaction;
+commit transaction; --uncommitted data is not saved in other connection
+
+rollback transaction; --undo
