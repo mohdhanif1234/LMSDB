b5ffc02bd7cdbb5361cd6cfd8bba9f0e3003f3a7
mohdhanif1234 <hanif.topia@gmail.com>
2021-12-07 15:17:43 +0530

    [Hanif]Add: Added a query to implement the different methods of cursors with variables

--- Cursors.sql
+++ Cursors.sql
@@ -19,3 +19,29 @@ close myCursor
 -- de-allocating a cursor
 deallocate myCursor
 
+-- Using a cursor with cursor variables
+-- declaring a cursor
+declare myCursor cursor scroll for select id, email from User_Details
+-- declaring a cursor variable
+declare @user_id int, @user_email varchar(50)
+-- opening a cursor
+open myCursor
+-- methods of cursors
+fetch first from myCursor into @user_id, @user_email
+print 'User is:' + cast(@user_id as varchar(50)) + ' ' + @user_email
+fetch next from myCursor into @user_id, @user_email
+print 'User is:' + cast(@user_id as varchar(50)) + ' ' + @user_email
+fetch last from myCursor into @user_id, @user_email
+print 'User is:' + cast(@user_id as varchar(50)) + ' ' + @user_email
+fetch prior from myCursor into @user_id, @user_email
+print 'User is:' + cast(@user_id as varchar(50)) + ' ' + @user_email
+fetch absolute 3 from myCursor into @user_id, @user_email
+print 'User is:' + cast(@user_id as varchar(50)) + ' ' + @user_email
+fetch relative 1 from myCursor into @user_id, @user_email
+print 'User is:' + cast(@user_id as varchar(50)) + ' ' + @user_email
+fetch relative -1 from myCursor into @user_id, @user_email
+print 'User is:' + cast(@user_id as varchar(50)) + ' ' + @user_email
+-- closing a cursor
+close myCursor
+-- de-allocating a cursor
+deallocate myCursor
