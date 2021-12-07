ba0f2da9eac656e9ce933777510b7e1c3e693b90
mohdhanif1234 <hanif.topia@gmail.com>
2021-12-08 00:41:48 +0530

    [Hanif]Add: Created a clustered index for a primary key Id

--- Indexes.sql
+++ Indexes.sql
@@ -5,3 +5,8 @@ create index ix_CpuLogData_CpuIdleTime
 on [CpuLogData2019-11-17-new] (Cpu_idle_Time asc);
 
 select * from [dbo].[CpuLogData2019-11-17-new] where Cpu_idle_Time > 100000 and Cpu_idle_Time < 200000;
+
+alter table [CpuLogData2019-11-17-new] add Id int primary key identity;
+
+create clustered index ix_CpuLogData_Id_Clustered
+on [CpuLogData2019-11-17-new] (Id asc);
